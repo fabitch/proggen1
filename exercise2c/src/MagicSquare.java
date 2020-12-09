611b6122e2c12b67e2295bbac5f6848637cf5e83
fabitch <fabian.s.dill@gmail.com>
2020-12-09 19:37:30 +0100

    initial commit

---
+++ exercise2c/src/MagicSquare.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MagicSquare {
+    int[][] square;
+
+    public MagicSquare(int[][] square) {
+        this.square = square;
+    }
+
+    public int[] showMagicNumbers(int k){
+        int[] tmp = {1,2};
+        return tmp;
+    }
+}
